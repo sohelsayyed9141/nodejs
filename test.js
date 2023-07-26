6e3890c78dd2db1f55f0008085382e08879bffd4
sohelsayyed <sayyed.sohel9141@gmail.com>
2023-07-26 17:06:06 +0530

    sayyed

--- test.js
+++ test.js
@@ -4,7 +4,7 @@ var app = require('../index.js');describe('GET /', function() {
 
  it('respond with hello world', function(done) { //navigate to root and check the the response is "hello sohelsayyed"
 
- request(app).get('/').expect('hello sohel ', done);
+ request(app).get('/').expect('hello sayyed sohel ', done);
 
  });
 
